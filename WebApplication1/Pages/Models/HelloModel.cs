a7ff7880256219ad0ece191912e5d0466fc81a88
Houmam <houmam.wazzeh@gmail.com>
2019-11-15 16:07:54 +0300

    master: extend Microsoft.AspNetCore.Mvc.RazorPages.Page
    - Partial view si not working

---
+++ WebApplication1/Pages/Models/HelloModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication1.Pages.Models
+{
+    public class HelloModel
+    {
+        public string Name { get; set; }
+
+        public HelloModel(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ WebApplication1/Razor/CustomRazorPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplication1.Razor
+{
+public abstract class CustomRazorPage : Microsoft.AspNetCore.Mvc.RazorPages.Page
+{
+    public string SayHi(string name)
+    {
+        return $"Hi <strong>{name}</strong>";
+    }
+}
+}
